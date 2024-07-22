281d9cef9867959de37a07117c89d78c6f7aa202
Evellin Moura <126713662+evellinmoura@users.noreply.github.com>
2024-07-22 10:38:52 -0300

    Update Tabuleiro.java

--- src/co/jogo/Tabuleiro.java
+++ src/co/jogo/Tabuleiro.java
@@ -22,12 +22,12 @@ public class Tabuleiro {
 
         if (posicao == 10 || posicao == 25 || posicao == 38) {
             System.out.println(jogador.getNome() + " não joga na próxima rodada.");
-            // Implementar lógica para pular a próxima rodada
+            // lógica pra pular a próxima rodada
         }
 
         if (posicao == 13) {
             System.out.println(jogador.getNome() + " caiu na casa surpresa!");
-            // Implementar lógica para tirar uma carta e mudar o tipo do jogador
+            // lógica pra tirar uma carta e mudar o tipo do jogador
         }
 
         if (posicao == 5 || posicao == 15 || posicao == 30) {
@@ -39,12 +39,12 @@ public class Tabuleiro {
 
         if (posicao == 17 || posicao == 27) {
             System.out.println(jogador.getNome() + " pode escolher um competidor para voltar ao início.");
-            // Implementar lógica para escolher um jogador para voltar ao início
+            // lógica pra escolher um jogador para o início
         }
 
         if (posicao == 20 || posicao == 35) {
             System.out.println(jogador.getNome() + " trocou de lugar com o jogador que está mais atrás.");
-            // Implementar lógica para trocar de lugar com o jogador que está mais atrás
+            // lógica que troca de lugar com o jogador que está mais atrás
         }
     }
 
