4de10e244ef9188ce82963a42560936bf1d17e85
Evellin Moura <126713662+evellinmoura@users.noreply.github.com>
2024-07-25 21:08:03 -0300

    Update Tabuleiro.java

--- src/co/jogo/Tabuleiro.java
+++ src/co/jogo/Tabuleiro.java
@@ -1,50 +1,47 @@
 package co.jogo;
 
-import co.jogador.JogadorAzarado;
 import co.jogador.Player;
 import java.util.List;
 
 public class Tabuleiro {
+    private Regras regras;
     private int[] casas;
 
     public Tabuleiro() {
         casas = new int[40];
-        // Inicialização das casas
+        regras = new Regras();
     }
 
-    public void moverJogador(Player jogador, int casas) {
+    public void moverJogador(Player jogador, List<Player> jogadores, int casas) {
         jogador.mover(casas);
-        aplicarRegras(jogador);
+        aplicarRegras(jogador, jogadores);
     }
 
-    private void aplicarRegras(Player jogador) {
+    private void aplicarRegras(Player jogador, List<Player> jogadores) {
         int posicao = jogador.getPosicao();
 
         if (posicao == 10 || posicao == 25 || posicao == 38) {
             System.out.println(jogador.getNome() + " não joga na próxima rodada.");
-            // lógica pra pular a próxima rodada
+           // regras.pularProximaRodada(jogador);
         }
 
         if (posicao == 13) {
             System.out.println(jogador.getNome() + " caiu na casa surpresa!");
-            // lógica pra tirar uma carta e mudar o tipo do jogador
+            regras.casaSurpresa(jogador);
         }
 
         if (posicao == 5 || posicao == 15 || posicao == 30) {
-            if (!(jogador instanceof JogadorAzarado)) {
-                System.out.println(jogador.getNome() + " avançou 3 casas.");
-                jogador.mover(3);
-            }
+            regras.avancaTresCasas(jogador);
         }
 
         if (posicao == 17 || posicao == 27) {
-            System.out.println(jogador.getNome() + " pode escolher um competidor para voltar ao início.");
-            // lógica pra escolher um jogador para o início
+            System.out.println(jogador.getNome() + " escolha um competidor para voltar ao início.");
+            regras.escolherJogadorParaIncicio(jogador, jogadores);
         }
 
         if (posicao == 20 || posicao == 35) {
             System.out.println(jogador.getNome() + " trocou de lugar com o jogador que está mais atrás.");
-            // lógica que troca de lugar com o jogador que está mais atrás
+            regras.trocarComOutroJogador(jogador, jogadores);
         }
     }
 
